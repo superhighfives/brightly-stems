30621fb2cec0f4f557951ce8c6e5ad18a3145894
Charlie Gleason <hi@charliegleason.com>
2012-06-16 01:35:49 +1000

    Added little arrows illustration

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,21 @@
-/* Author:
+/* Author: Charlie Gleason
 
 */
 
+(function($) {
 
+  var Parallax = {
+    initialize: function() {
+      $(window).bind("scroll", Parallax.scrolling);
+      Parallax.scrolling();
+    },
+    scrolling: function() {
+      var fold = $(window).scrollTop();
+      var speed = fold * 0.25;
+      $(".arrows").css({top: 0 + speed});
+    }
+  };
 
+  Parallax.initialize();
 
-
+})(jQuery);
